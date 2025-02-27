67be03f0e1f755421404494e756f21a52387a4da
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2025-02-27 10:57:19 -0700

    10:57 checkin

---
+++ HogWild/HogWildWeb/Components/Pages/SamplePages/WorkingVersions.razor.cs
@@ -0,0 +1,55 @@
+﻿using HogWildSystem.BLL;
+using HogWildSystem.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace HogWildWeb.Components.Pages.SamplePages
+{
+    public partial class WorkingVersions
+    {
+        #region Fields
+        //  Property for holding any feedback messages
+        private string feedback = string.Empty;
+        // This private field holds a reference to the WorkingVersionsView instance.
+        private WorkingVersionsView? workingVersionView = new WorkingVersionsView();
+        #endregion
+
+        #region Properties
+        // This attribute marks the property for dependency injection.
+        [Inject]
+        // This property provides access to the 'WorkingVersionService' service.
+        protected WorkingVersionsService WorkingVersionService { get; set; } = default!;
+        #endregion
+
+        #region Methods
+        private void GetWoringVersions()
+        {
+            try
+            {
+                workingVersionView = WorkingVersionService.GetWorkingVersion();
+            }
+            #region catch all exceptions
+            catch (AggregateException ex)
+            {
+                foreach (var error in ex.InnerExceptions)
+                {
+                    feedback = error.Message;
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                feedback = GetInnerException(ex).Message;
+            }
+            catch (Exception ex)
+            {
+                feedback = GetInnerException(ex).Message;
+            }
+            #endregion
+        }
+        public Exception GetInnerException(System.Exception ex)
+        {
+            while (ex.InnerException != null)
+                ex = ex.InnerException;
+            return ex;
+        }
+    }
+}
